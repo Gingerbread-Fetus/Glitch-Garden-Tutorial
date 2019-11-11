1047e133c7df41510c94a6abe565567b990682b8
Hayden Matthews <haydenamatthews@yahoo.com>
2019-11-11 15:26:38 -0700

    Video 136: Got projectile to move and rotate.

---
+++ Glitch Garden/Glitch Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float rotationSpeed;
+    
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime, Space.World);
+    }
+}
