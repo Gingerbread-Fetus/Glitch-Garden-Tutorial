15be095dee44f09b9c01cc5fb8147f221b351f8e
Hayden Matthews <haydenamatthews@yahoo.com>
2019-11-13 14:48:57 -0700

    Got up to spawning defenders.

--- Glitch Garden/Glitch Garden/Assets/Scripts/Projectile.cs
+++ Glitch Garden/Glitch Garden/Assets/Scripts/Projectile.cs
@@ -6,10 +6,24 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float damage = 10f;
     
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime, Space.World);
         transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        var attacker = otherCollider.GetComponent<Attacker>();
+        
+        if(attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+
     }
 }
