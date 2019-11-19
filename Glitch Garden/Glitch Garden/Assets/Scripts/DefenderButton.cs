c854a84b16bb153acf16d3abef267eea67fa9ada
Hayden Matthews <haydenamatthews@yahoo.com>
2019-11-18 17:13:20 -0700

    Up to video 145. Added the start to resource system. Need to do Parent, Child & animation video.

---
+++ Glitch Garden/Glitch Garden/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DefenderButton : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] Defender defenderPrefab;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<Image>().color = new Color32(19, 15, 15, 255);
+        }
+        GetComponent<Image>().color = Color.white;
+        FindObjectOfType<DefenderSpawner>().SetSelectedDefender(defenderPrefab);
+    }
+}
